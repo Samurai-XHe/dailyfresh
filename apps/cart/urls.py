cfc48a8495f0c84112fe82379d1d01cfecd04691
Samurai-XHe <847834358@qq.com>
2018-11-26 12:12:04 +0900

    购物车更新和删除

--- apps/cart/urls.py
+++ apps/cart/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import CartAddView, CartInfoView
+from .views import CartAddView, CartInfoView, CartUpdateView, CartDeleteView
 
 
 app_name = 'cart'
 urlpatterns = [
     path('add/', CartAddView.as_view(), name='add'),  # 添加购物车记录
     path('', CartInfoView.as_view(), name='show'),  # 购物车页面显示
+    path('update/', CartUpdateView.as_view(), name='update'),  # 购物车记录更新
+    path('delete/', CartDeleteView.as_view(), name='delete'),  # 购物车记录删除
 ]
