847296556a504ee175088e51b257ae70a7c7906a
Samurai-XHe <847834358@qq.com>
2018-12-03 15:25:12 +0800

    用户支付成功后自动跳转

--- apps/order/urls.py
+++ apps/order/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import OrderPlaceView, OrderCommitView, OrderPayView
+from .views import OrderPlaceView, OrderCommitView, OrderPayView, CheckPayView, CommentView
 
 
 app_name = 'order'
 urlpatterns = [
     path('place/', OrderPlaceView.as_view(), name='place'),  # 提交订单页面显示
     path('commit/', OrderCommitView.as_view(), name='commit'),  # 订单创建
-    path('pay/', OrderPayView.as_view(), name='pay')
+    path('pay/', OrderPayView.as_view(), name='pay'),  # 订单支付
+    path('check/', CheckPayView.as_view(), name='check'),  # 查询支付结果
+    path('comment/<int:order_id>/', CommentView.as_view(), name='comment'),  # 订单评论
 ]
